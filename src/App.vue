35f19383933c7d2051c488d91101154318832d30
lqmzp <2792643139@qq.com>
2022-09-06 18:12:24 +0800

    登录页完成一半

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
-  <div class="toutiao toutiao-gengduo">
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <div class="box">按钮</div>
+  <div>
+    <router-view></router-view>
   </div>
 </template>
 
