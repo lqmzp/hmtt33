c7a573100e1fa598f2bda8bf0902105a2dc3d0fa
lqmzp <2792643139@qq.com>
2022-09-06 16:13:04 +0800

    配置完毕

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <div></div>
+  <div class="toutiao toutiao-gengduo">
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <div class="box">按钮</div>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style scoped>
+.box {
+  width: 700px;
+  height: 50px;
+  background-color: red;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
+import '@/assets/fonts/iconfont.css'
+import 'amfe-flexible'
 Vue.config.productionTip = false
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+Vue.use(Vant)
+new Vue({ Vant, router, store, render: (h) => h(App) }).$mount('#app')
