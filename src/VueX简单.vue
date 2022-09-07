4b302c9f9cf4232a1366fa662774ac1f6e5884c2
lqmzp <2792643139@qq.com>
2022-09-07 17:17:38 +0800

    feat：登录页完成

---
+++ src/VueX简单.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <!-- <router-view></router-view> -->
+    <h1>{{ count }}</h1>
+    <h1>{{ a }}</h1>
+    <h1>{{ bigNum }}</h1>
+    <button @click="SET_COUNT(20)">点击+1</button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters, mapMutations } from 'vuex'
+export default {
+  methods: {
+    // increment() {
+    //   this.$store.commit('SET_COUNT', 10)
+    //   console.log(this)
+    // },
+    ...mapMutations(['SET_COUNT'])
+  },
+  computed: {
+    ...mapState(['count', 'a']),
+    ...mapGetters(['bigNum'])
+  }
+}
+</script>
+
+<style scoped>
+.box {
+  width: 700px;
+  height: 50px;
+  background-color: red;
+}
+</style>
---
+++ src/api/user.js
@@ -0,0 +1,27 @@
+// api文件根据后端接口文档划分
+import request from '@/utils/request'
+/**
+ *
+ * @param {Number} mobile 手机号
+ * @param {Number} code 验证码
+ * @returns Promise
+ */
+export const login = (mobile, code) => {
+  return request({
+    url: '/v1_0/authorizations',
+    method: 'POST',
+    data: {
+      mobile,
+      code
+    }
+  })
+}
+
+export const sendCodeApi = (a) => {
+  return request({
+    url: `/v1_0/sms/codes/${a}`,
+    data: {
+      a
+    }
+  })
+}
